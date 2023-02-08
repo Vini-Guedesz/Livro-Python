82e10d23279438e6027ba25eb6db0264ae3bab8d
Vinicius Guedes <123116030+Vini-Guedesz@users.noreply.github.com>
2023-02-08 14:46:11 -0300

    update cap 7

---
+++ cap 7/exercício_7.07.py
@@ -0,0 +1,8 @@
+"""
+infinito: Escreva um laço que nunca termine e execute-o. (Para encerrar o
+laço, pressione CTRL-C ou feche a janela que está exibindo a saída.)
+"""
+num = 1
+while True:
+    num = num + 1
+    print(num)
