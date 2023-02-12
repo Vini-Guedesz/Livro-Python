b3335d386ff95fbc5f0c57b227999b2a31d90c7a
Vinicius Guedes <123116030+Vini-Guedesz@users.noreply.github.com>
2023-02-12 19:01:09 -0300

    update cap 8

---
+++ cap 8/exercício_8.02.py
@@ -0,0 +1,10 @@
+"""
+Livro favorito: Escreva uma função chamada favorite_book() que aceite
+um parâmetro title. A função deve exibir uma mensagem como Um dos meus
+livros favoritos é Alice no país das maravilhas. Chame a função e não se
+esqueça de incluir o título do livro como argumento na chamada da função.
+"""
+def favorite_book(título):
+    print('meu livro favorito é ' + título + '!')
+
+favorite_book('peter pan')
