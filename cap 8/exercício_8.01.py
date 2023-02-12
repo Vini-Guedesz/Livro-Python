3fbcd2bd22dd6e0ebfa155e3c0158b1b42b825f5
Vinicius Guedes <123116030+Vini-Guedesz@users.noreply.github.com>
2023-02-12 18:11:31 -0300

    add cap 8

---
+++ cap 8/exercício_8.01.py
@@ -0,0 +1,9 @@
+"""
+Mensagem: Escreva uma função chamada display_message() que mostre
+uma frase informando a todos o que você está aprendendo neste capítulo.
+Chame a função e certifique-se de que a mensagem seja exibida corretamente.
+"""
+def display_messsage():
+    print('neste capítulo estou aprendendo sobre funções em python')
+
+display_messsage()
